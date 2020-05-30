235780cecdd4bfa4e4cab3179736a400f034e02d
hosseinghaedi1999@yahoo.com <Admin5525181>
2020-05-30 23:28:56 +0400

    Initial Commit

---
+++ smack-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack-clone
+//
+//  Created by Hossein Ghaedi on 30/05/2020.
+//  Copyright © 2020 Hossein Ghaedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
